364a80eb07a65ed468b9ae58f779243338b92d5a
MinHtutZaw <minhtutzaw16@gmail.com>
2022-11-11 09:36:30 +0630

    Changes Into DataBase System

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -6,7 +6,7 @@
     @foreach ($blogs as $blog)
 
     <h1>
-       <a href="blogs/{{$blog->slug}}"> {{$blog->title}} </a>
+       <a href="blogs/{{$blog->id}}"> {{$blog->title}} </a>
      </h1>
   <div>
     <p>  published at-  {{$blog->date}} </p>
