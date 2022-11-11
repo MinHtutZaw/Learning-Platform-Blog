8ab8d78f6efa514025a7d871ea3ddfcd5e6a5308
MinHtutZaw <minhtutzaw16@gmail.com>
2022-11-11 22:28:35 +0630

    belongsTo Relationship

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Blog extends Model
 {
     use HasFactory;
-    protected $fillable=['title','intro','body'];
+    protected $fillable = ['title', 'intro', 'body'];
     ///protected $guarded=[];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -8,6 +8,7 @@
     <h1>
        <a href="blogs/{{$blog->slug}}"> {{$blog->title}} </a>
      </h1>
+     <p>category -  <a href="">{{$blog->category->name}}</a></p>
   <div>
     <p>  published at-  {{$blog->created_at->diffForHumans()}} </p>
      <p>   {{$blog->intro}}   </p>
