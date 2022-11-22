b702c6dee9d0d7f7c982e7abbff7b59344560f40
MinHtutZaw <minhtutzaw16@gmail.com>
2022-11-22 12:45:38 +0630

    search feature (conditional query)

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
+    $blogs=Blog::latest();
+    if(request('search')){
+        $blogs=$blogs->where('title','LIKE','%'.request('search').'%');
+    }
     return view('blogs',[
-        "blogs"=>Blog::latest()->get(), // eager load lazy loading 
+        "blogs"=>$blogs->get(), // eager load lazy loading 
         "categories"=>Category::all()
     ]);
 });
