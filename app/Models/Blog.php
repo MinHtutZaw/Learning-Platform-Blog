85e7936fbd02cb57b4331b545fa79a021b4c7fb6
MinHtutZaw <minhtutzaw16@gmail.com>
2022-11-26 15:08:43 +0630

    Logical Grouping

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -16,12 +16,18 @@ class Blog extends Model
     protected $with=['category','author'];
 
     public function scopeFilter($query,$filter){
-        $query->when($filter['search'], function($query,$search){
-        
+        $query->when($filter['search']??false, function($query,$search){
+             
+             $query->where(function($query)  use ($search){
+                $query->where('title','LIKE','%'.$search.'%')
+                     ->orWhere('body', 'LIKE','%'.$search.'%');
+             }
 
-            $query->where('title','LIKE','%'.$search.'%')
-                       ->orWhere('body', 'LIKE', '%'.$search.'%');
-            
+             );
+
+
+
+           
        
         });
     }
