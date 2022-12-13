39b32638af4e84188029d967db0b5e3d0b87453b
MinHtutZaw <minhtutzaw16@gmail.com>
2022-12-14 03:29:27 +0630

    refactor

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -22,39 +22,20 @@
     </div>
     <section class="container">
         <div class="col-md-8 mx-auto">
-        @auth
-            <x-card-wrapper
-                class="bg-secondary"
-                
-            >
-                <form
-                action="/blogs/{{$blog->slug}}/comments"
-                    method="POST">
-                    @csrf
-                    <div class="mb-3">
-                        <textarea
-                            name="body"
-                            cols="10"
-                            class="form-control border border-0"
-                            rows="5"
-                            placeholder="say something..."
-                        ></textarea>
-                      <x-error name="body"></x-error>
-                    </div>
-                    <div class="d-flex justify-content-end">
-                        <button
-                            type="submit"
-                            class="btn btn-primary"
-                        >Submit</button>
-                    </div>
-                </form>
-                @else
-            <p class="text-center">please <a href="/login">login</a> to participate in this discussion.</p>
-            @endauth
-            </x-card-wrapper>
+          @auth
+               <x-comment-form :blog="$blog" />
+          @else
+          <p class="text-center">please <a href="/login">login</a> to participate in this discussion.</p>
+          @endauth
+            
         </div>
     </section>
-    <x-comment :comments="$blog->comments"/>
+  
+    
+    @if($blog->comments->count())
+    <x-comments :comments="$blog->comments" />
+    @endif
+   
     <x-subscribe/>
     <x-blogs_you_may_like  :randomBlogs='$randomBlogs'  />
  
